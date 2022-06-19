a9e8a6e84d899dd7fa59721107984230484a2b25
Augusto Britez <45775681+Wieworki@users.noreply.github.com>
2022-06-19 14:24:52 -0300

    Update PalindromeChecker.js

--- PalindromeChecker.js
+++ PalindromeChecker.js
@@ -8,9 +8,24 @@ We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR 
 
 We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.*/
 
-
 function palindrome(str) {
+  str = onlyAlphaNumeric(str);
+  let aux = str.length-1;             //Last element
+  for(let i = 0; i < str.length; i++){  //Characters check
+    if(str[i] != str[aux]){
+      return false
+    }
+    aux--;
+  }
   return true;
 }
 
-palindrome("eye");
+function onlyAlphaNumeric(str){
+  let nonAlphaNumericRegEx = /[A-Za-z0-9]/ig;          //Only alphanumerics characters
+  str = str.match(nonAlphaNumericRegEx,""); //We extract them
+  str = str.join("");         //Array to string
+  str = str.toLowerCase();    
+  return str;
+}
+
+palindrome("_eye");
